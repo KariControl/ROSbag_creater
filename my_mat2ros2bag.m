51f6ce64164ccfd9aada75ba5c672e7a1c880fd4
制御系エンジニア <118587431+KariControl@users.noreply.github.com>
2024-12-17 20:39:37 +0900

    Update my_mat2ros2bag.m

--- my_mat2ros2bag.m
+++ my_mat2ros2bag.m
@@ -6,7 +6,6 @@ for i=1:4501
     message2.header.stamp.nanosec=uint32((tt(i)-fix(tt(i)))*10^9);
     message2.twist.linear.x=vv(i);
     message2.twist.angular.z=ref_m(i,2);
-%    write(bagWriter, "MotionPlanning", ros2time(message2.header.stamp.sec,message2.header.stamp.nanosec),message2);
     write(bagWriter, "MotionPlanning", ros2time(tt(i)),message2);
 end
 delete(bagWriter);
