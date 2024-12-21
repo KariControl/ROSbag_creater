7059661b1ee15e98fc15212149e88f5fdb2c56a5
制御系エンジニア <118587431+KariControl@users.noreply.github.com>
2024-12-21 14:56:49 +0900

    Update my_mat2ros2bag.m

--- my_mat2ros2bag.m
+++ my_mat2ros2bag.m
@@ -1,12 +1,18 @@
-load('matlab.mat');
-bagWriter = ros2bagwriter("bag_files4/my_bag_file4");
+%% テスト用入力信号の生成
+tt=0:0.01:20;
+f=0.1;
+y1=sin(tt*2*pi*f);
+y2=cos(tt*2*pi*f);
 
-for i=1:4501
-    message2 = ros2message("geometry_msgs/TwistStamped");
-    message2.header.stamp.sec=int32(tt(i));
-    message2.header.stamp.nanosec=uint32((tt(i)-fix(tt(i)))*10^9);
-    message2.twist.linear.x=vv(i);
-    message2.twist.angular.z=ref_m(i,2);
-    write(bagWriter, "MotionPlanning", ros2time(tt(i)),message2);
+%% ROSbagへの変換処理
+bagWriter = ros2bagwriter("bag_files4/my_bag_file4");% ROSbagオブジェクト生成
+
+for i=1:length(y1)
+    message2 = ros2message("geometry_msgs/TwistStamped");% メッセージ型定義
+    message2.header.stamp.sec=int32(tt(i));% タイムスタンプ打刻
+    message2.header.stamp.nanosec=uint32((tt(i)-fix(tt(i)))*10^9);% タイムスタンプ打刻
+    message2.twist.linear.x=y1(i);
+    message2.twist.linear.y=y2(i);
+    write(bagWriter, "/matlab/control", ros2time(tt(i)),message2);% データ書き込み
 end
-delete(bagWriter);
+delete(bagWriter);% オブジェクト削除によるメタデータ生成
